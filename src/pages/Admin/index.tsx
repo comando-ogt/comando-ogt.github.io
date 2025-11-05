215963fa31c6160f9c5abdb14800f7e6bf6e409f
KeSuave <kesuave79@gmail.com>
2025-11-04 21:31:56 -0800

    feat: icons for admin

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,31 +1,48 @@
+import { faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
 
 import { Button } from "../../components/Button";
 import type { DBEvent } from "../../types/events";
 import { EventModal } from "../../modals/Event";
 import { FlagBar } from "../../components/FlagBar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "../../components/Link";
 import { Navigate } from "react-router";
 import { OGTEvent } from "../../components/OGTEvent";
 import { RegularPageLayout } from "../../layouts/RegularPage";
 import { supabase } from "../../supabase";
 import { useAuthStore } from "../../store/auth";
+import { useEventsStore } from "../../store/events";
+import { useMembersStore } from "../../store/members";
 
 export function AdminPage() {
-  const [events, setEvents] = useState<DBEvent[]>([]);
   const [isEventModalOpen, setIsEventModalOpen] = useState(false);
   const [modalEvent, setModalEvent] = useState<DBEvent | undefined>(undefined);
+
+  const members = useMembersStore((s) => s.members);
+  const membersLoading = useMembersStore((s) => s.isLoading);
+  const getMembers = useMembersStore((s) => s.getMembers);
+  const subscribeToMembers = useMembersStore((s) => s.subscribeToMembers);
+
+  const events = useEventsStore((s) => s.events);
+  const eventsLoading = useEventsStore((s) => s.isLoading);
+  const getEvents = useEventsStore((s) => s.getEvents);
+  const subscribeToEvents = useEventsStore((s) => s.subscribeToEvents);
 
   const profile = useAuthStore((s) => s.profile);
 
   useEffect(() => {
-    getEvents();
+    if (members.length === 0) {
+      getMembers();
+    }
+
+    if (events.length === 0) {
+      getEvents();
+    }
+
+    subscribeToMembers();
+    subscribeToEvents();
   }, []);
-
-  async function getEvents() {
-    const { data } = await supabase.from("events").select();
-
-    setEvents(data as unknown as DBEvent[]);
-  }
 
   if (!profile || profile?.web_role !== "admin") {
     return <Navigate to="/404" replace />;
@@ -40,13 +57,34 @@ export function AdminPage() {
         <div className="flex justify-between items-center mb-6">
           <h2 className="font-staatliches text-white text-4xl">Miembros</h2>
         </div>
-        <div className="space-y-2">
-          {/* {members.map(member => (
-                                <div key={member.name} className="flex justify-between items-center bg-black/30 p-3 rounded-lg">
-                                    <span>{member.name} - ({member.role})</span>
-                                    <button onClick={() => handleRemoveMember(member.name)} className="text-red-500 hover:text-red-400">Remove</button>
-                                </div>
-                            ))} */}
+        <div className="flex flex-wrap justify-center gap-2">
+          {membersLoading && (
+            <>
+              {Array.from({ length: 10 }).map((_, i) => (
+                <div
+                  key={i}
+                  className="flex flex-col items-center gap-2 bg-neutral-950 p-3 rounded-lg min-w-3xs min-h-24 animate-pulse"
+                />
+              ))}
+            </>
+          )}
+          {members.map((member) => (
+            <div
+              key={member.id}
+              className="flex flex-col items-center gap-2 bg-neutral-950 p-3 rounded-lg min-w-3xs"
+            >
+              <Link to={`/miembro/${member.url}`}>{member.name}</Link>
+              <div className="flex gap-2">
+                <Button variant="gray">
+                  <FontAwesomeIcon icon={faPencil} />
+                </Button>
+
+                <Button variant="red">
+                  <FontAwesomeIcon icon={faTrash} />
+                </Button>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
 
@@ -65,6 +103,17 @@ export function AdminPage() {
           </Button>
         </div>
         <div className="space-y-2">
+          {eventsLoading && (
+            <>
+              {Array.from({ length: 4 }).map((_, i) => (
+                <div
+                  key={i}
+                  className="flex gap-2 bg-neutral-950 rounded min-h-30 animate-pulse"
+                />
+              ))}
+            </>
+          )}
+
           {events.map((event) => (
             <div key={event.id} className="flex gap-2 rounded">
               <OGTEvent event={event} className="flex-1" full />
@@ -76,7 +125,7 @@ export function AdminPage() {
                     setIsEventModalOpen(true);
                   }}
                 >
-                  Editar
+                  <FontAwesomeIcon icon={faPencil} />
                 </Button>
 
                 <Button
@@ -90,15 +139,13 @@ export function AdminPage() {
                     if (response.status === 204) {
                       // TODO: create a better ui
                       alert("Evento eliminado con éxito.");
-
-                      getEvents();
                     } else {
                       // TODO: create a better ui
                       alert("Error al eliminar evento.");
                     }
                   }}
                 >
-                  Borrar
+                  <FontAwesomeIcon icon={faTrash} />
                 </Button>
               </div>
             </div>
